e55e7c26913edf4b7e85837e32152f07754e4aaa
Jayani22 <mutcherlajayani@gmail.com>
2026-02-24 16:29:35 +0530

    Blue Green setup

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,11 +5,3 @@ output "cluster_name" {
 output "service_name" {
     value = module.ecs.service_name
 }
-
-output "codedeploy_app_name" {
-    value = module.codedeploy.app_name
-}
-
-output "deployment_group_name" {
-    value = module.codedeploy.deployment_group_name
-}
