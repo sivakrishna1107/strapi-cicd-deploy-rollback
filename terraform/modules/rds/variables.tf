2ebff0f669e4ec97f1c83b7b29015296e080d0e3
Jayani22 <mutcherlajayani@gmail.com>
2026-02-24 14:04:53 +0530

    Blue Green setup

---
+++ terraform/modules/rds/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+    type = string
+}
+
+variable "subnet_ids" {
+    type = list(string)
+}
+
+variable "ecs_sg_id" {
+    type = string
+}
+
+variable "db_password" {
+    type = string
+}
