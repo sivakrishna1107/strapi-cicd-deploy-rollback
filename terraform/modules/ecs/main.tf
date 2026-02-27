d24c453d6af0757cb57290875869aac6708e21e0
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-27 15:20:45 +0530

    Update main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -87,7 +87,7 @@ resource "aws_ecs_task_definition" "this" {
       logConfiguration = {
           logDriver = "awslogs"
           options = {
-            awslogs-group         = "/ecs/${var.project_name}-t11-siva"
+            awslogs-group         = "/ecs/${var.project_name}-siva-t11"
             awslogs-region        = var.aws_region
             awslogs-stream-prefix = "ecs"
           }
