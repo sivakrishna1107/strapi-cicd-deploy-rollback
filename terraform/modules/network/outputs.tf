c0fa9ebd74a316ff2d4f3e91353b9826326331c3
sivakrishna1107 <sivakrishna1107@gmail.com>
2026-02-28 13:10:13 +0530

    Update outputs.tf

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,3 +1,7 @@
+#output "vpc_id"value = data.aws_vpc.default.id
+#output "subnet_ids"
+#value = data.aws_subnets.default.ids
+
 output "vpc_id" {
   value = data.aws_vpc.default.id
 }
@@ -5,7 +9,6 @@ output "vpc_id" {
 output "subnet_ids" {
   value = data.aws_subnets.default.ids
 }
-
 output "alb_sg_id" {
   value = aws_security_group.alb_sg.id
 }
